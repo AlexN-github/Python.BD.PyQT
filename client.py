d24359b6e0e6f447f3f8258aec07a28972d50386
Alex <n_alex2001@mail.ru>
2020-07-24 14:51:40 +0300

    Выполнил перевод на классы

--- client.py
+++ client.py
@@ -14,119 +14,158 @@ def create_parser():
     parser.add_argument('-n', '--name', default='')
     parser.add_argument('-a', '--addr', default='localhost')
     parser.add_argument('-p', '--port', default=str(default_port))
-
     return parser
 
 
-def execute_command_presence():
-    logger.info('Выполняем команду: `presence`')
-    # print('Выполняем команду: `presence`')
-    command = {
-        'action': 'presence',
-        'time': int(time.time()),
-        'type': 'status',
-        'user': {
-            'account_name': account_name,
-            'status': 'Yep, I am here!'
+# Основной класс клиента
+class Client:
+    def __init__(self, listen_address, listen_port, client_name):
+
+        # Параментры подключения
+        self.addr = listen_address
+        self.port = int(listen_port)
+
+        # Имя клиента
+        self.account_name = client_name
+
+    def execute_command_presence(self):
+        logger.info('Выполняем команду: `presence`')
+        # print('Выполняем команду: `presence`')
+        command = {
+            'action': 'presence',
+            'time': int(time.time()),
+            'type': 'status',
+            'user': {
+                'account_name': '',
+                'status': 'Yep, I am here!'
+            }
         }
-    }
-    timestamp = int(time.time())
-    command['time'] = timestamp
-    command['account_name'] = 'Alex'
-    result = execute_command(command)
-    print(result)
+        timestamp = int(time.time())
+        command['time'] = timestamp
+        command['account_name'] = self.account_name
+        result = self.execute_command(command)
+        print(result)
 
-    return result
+        return result
 
-def execute_command_broadcast():
-    logger.info('Выполняем команду: `broadcast`')
-    # print('Выполняем команду: `broadcast`')
-    command = {
-        'action': 'broadcast',
-        'time': int(time.time()),
-        'type': 'status',
-        'user': {
-            'account_name': account_name,
-            'status': 'Yep, I am here!'
+    def execute_command_broadcast(self):
+        logger.info('Выполняем команду: `broadcast`')
+        # print('Выполняем команду: `broadcast`')
+        command = {
+            'action': 'broadcast',
+            'time': int(time.time()),
+            'type': 'status',
+            'user': {
+                'account_name': '',
+                'status': 'Yep, I am here!'
+            }
         }
-    }
-    timestamp = int(time.time())
-    command['time'] = timestamp
-    command['account_name'] = 'Alex'
-    result = execute_command(command)
-    print(result)
+        timestamp = int(time.time())
+        command['time'] = timestamp
+        command['account_name'] = self.account_name
+        result = self.execute_command(command)
+        print(result)
 
-    return result
-
-
-def waiting_messages():
-    def parsing_recv(msg):
-        respond = json.loads(msg)
-        return respond
-
-    data = sock.recv(block_transfer_size)
-    msg_recv = data.decode('utf-8')
-    result = parsing_recv(msg_recv)
-    logger.info('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
-    print(result)
-    #print('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
-    return result
+        return result
 
 
-def connect_to_server(addr, port):
-    global sock
-    logger.info('Устанавливаем соединение: {0}'.format((addr, port)))
-    # print('Устанавливаем соединение:', (addr, port))
-    sock = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
-    sock.connect((addr, port))  # Соединиться с сервером
+    def waiting_messages(self):
+        def parsing_recv(msg):
+            respond = json.loads(msg)
+            return respond
+
+        data = sock.recv(block_transfer_size)
+        msg_recv = data.decode('utf-8')
+        result = parsing_recv(msg_recv)
+        logger.info('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
+        print(result)
+        #print('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
+        return result
 
 
-def disconnect_from_server():
-    logger.info('Отключаемся от сервера {0}'.format((addr, port)))
-    # print('Отключаемся от сервера', (addr, port))
-    sock.close()
+    def connect_to_server(self):
+        global sock
+        logger.info('Устанавливаем соединение: {0}'.format((self.addr, self.port)))
+        # print('Устанавливаем соединение:', (addr, port))
+        sock = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
+        sock.connect((self.addr, self.port))  # Соединиться с сервером
 
 
-def execute_command(msg):
-    def parsing_recv(msg):
-        respond = json.loads(msg)
-        return respond
-
-    msg_send = json.dumps(msg)
-    print(msg_send)
-    sock.send(msg_send.encode('utf-8'))
-    data = sock.recv(block_transfer_size)
-    msg_recv = data.decode('utf-8')
-    result = parsing_recv(msg_recv)
-    logger.info('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
-    # print('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
-    return result
+    def disconnect_from_server(self):
+        logger.info('Отключаемся от сервера {0}'.format((self.addr, self.port)))
+        # print('Отключаемся от сервера', (addr, port))
+        sock.close()
 
 
-logger = logging.getLogger('app.client')
-logger.info('Программа клиент запущена')
-parser = create_parser()
-namespace = parser.parse_args(sys.argv[1:])
-client_name = namespace.name
-print(client_name)
-addr = namespace.addr  # 'localhost'
-port = int(namespace.port)
-account_name = 'Alex'
+    def execute_command(self, msg):
+        def parsing_recv(msg):
+            respond = json.loads(msg)
+            return respond
+
+        msg_send = json.dumps(msg)
+        print(msg_send)
+        sock.send(msg_send.encode('utf-8'))
+        data = sock.recv(block_transfer_size)
+        msg_recv = data.decode('utf-8')
+        result = parsing_recv(msg_recv)
+        logger.info('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
+        # print('Сообщение от сервера: {0}; Code: {1}'.format(result['msg'], result['code']))
+        return result
+
+
+    def mainloop(self):
+        try:
+            self.connect_to_server()
+            while True:
+                print('*' * 30)
+                inputuser = input(
+                    'Отправить клиенту команду PRESENCE (1) \r\nОтправить клиенту команду BROADCAST (2) \r\nЖдем входящие сообшения (3) \r\nОтключиться от сервера и выйти (q)')
+                if inputuser == '1':
+                    self.execute_command_presence()
+                if inputuser == '2':
+                    self.execute_command_broadcast()
+                if inputuser == '3':
+                    self.waiting_messages()
+                elif inputuser == 'q':
+                    break
+
+            # disconnect_from_server()
+        except Exception as err:
+            logger.error(err)
+
+
+def main():
+
+    # Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    # Создание экземпляра класса - клиента.
+    client = Client(namespace.addr, namespace.port, namespace.name)
+    client.mainloop()
+
+
+#parser = create_parser()
+#namespace = parser.parse_args(sys.argv[1:])
+#client_name = namespace.name
+#print(client_name)
+#addr = namespace.addr  # 'localhost'
+#port = int(namespace.port)
+#account_name = 'Alex'
 
 try:
-    connect_to_server(addr=addr, port=port)
-    while True:
-        print('*'*30)
-        inputuser = input('Отправить клиенту команду PRESENCE (1) \r\nОтправить клиенту команду BROADCAST (2) \r\nЖдем входящие сообшения (3) \r\nОтключиться от сервера и выйти (q)')
-        if inputuser == '1':
-            execute_command_presence()
-        if inputuser == '2':
-            execute_command_broadcast()
-        if inputuser == '3':
-            waiting_messages()
-        elif inputuser == 'q':
-            break
+    # Инициализируем логирование
+    logger = logging.getLogger('app.client')
+    logger.info('Программа клиент запущена')
 
-    #disconnect_from_server()
+    if __name__ == '__main__':
+        main()
+
+
 except Exception as err:
-    logger.error(err)
+    if logger:
+        logger.error(err)
+    else:
+        print(err)
+
+
--- server.py
+++ server.py
@@ -10,7 +10,6 @@ import sys
 
 from common.variables import *
 
-
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--addr', default=str(server_addr_default))
@@ -19,144 +18,172 @@ def create_parser():
     return parser
 
 
-def execute_command_presence(client, command):
-    result = {}
-    result['msg'] = 'command `{0}` completed successfully'.format(command['action'])
-    result['code'] = 200
-    sending_responde(client, result)
-    return result
+# Основной класс сервера
+class Server:
+    def __init__(self, listen_address, listen_port):
 
-def execute_command_broadcast(client, command):
-    result = {}
-    result['msg'] = 'command `{0}` completed successfully'.format(command['action'])
-    result['code'] = 200
-    sending_responde_brodcaste(client, result)
-    return result
+        # Параментры подключения
+        self.addr = listen_address
+        self.port = int(listen_port)
 
-def processing_command(request):
-    def parsing_command(msg):
-        command = json.loads(msg)
-        return command
+        # Список подключённых клиентов.
+        self.clients = []
 
-    def execute_command(command):
-        action = command['action']
-        if action == 'presence':
-            result = execute_command_presence(request, command)
-        if action == 'broadcast':
-            result = execute_command_broadcast(request, command)
-        elif action == '...':
-            pass
-        else:
-            result = {}
-            result['msg'] = 'Invalid command'
-            result['code'] = 400
+        #Очередь запросов на обработку
+        self.request_queue = {}
+
+    def execute_command_presence(self, client, command):
+        result = {}
+        result['msg'] = 'command `{0}` completed successfully'.format(command['action'])
+        result['code'] = 200
+        self.sending_responde(client, result)
         return result
 
-    print('request', request)
-    msg = request_queue[request]#request.recv(block_transfer_size).decode('utf-8')
-    print('msg', msg)
-    #addr = request.raddr
-    command = parsing_command(msg)
-    logger.debug('Принято сообщение: {0}, от клиента: {1}'.format(msg, addr))
-    logger.info('Принята команда: {0}'.format(command['action']))
-    return execute_command(command)
+    def execute_command_broadcast(self, client, command):
+        result = {}
+        result['msg'] = 'command `{0}` completed successfully'.format(command['action'])
+        result['code'] = 200
+        self.sending_responde_brodcaste(client, result)
+        return result
 
+    def processing_command(self, request):
+        def parsing_command(msg):
+            command = json.loads(msg)
+            return command
 
-def sending_responde(client, result):
-    logger.info(
-        'Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
-    # print('Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
-    msg_send = json.dumps(result)
-    client.send(msg_send.encode('utf-8'))
-    #client.close()
-
-def sending_responde_brodcaste(client, result):
-    logger.info(
-        'Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
-    # print('Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
-    msg_send = json.dumps(result)
-    for cli in clients:
-        cli.send(msg_send.encode('utf-8'))
-    #client.close()
-
-
-def read_requests(r_clients):
-    """ Чтение запросов из списка клиентов
-    """
-
-    for sock in r_clients:
-        try:
-            data = sock.recv(1024).decode('utf-8')
-            request_queue[sock] = data
-        except:
-            print('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
-            logger.info('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
-            clients.remove(sock)
-
-    #return responses
-
-
-def queue_processing():
-    """ Обрабатываем очередь запросов
-    """
-    while request_queue != {}:
-        request = next(iter(request_queue)) #request_queue[list(request_queue.items())[0]]
-        processing_command(request)
-        request_queue.pop(request, None)
-        #del request_queue[request]
-
-def mainloop(addr, port):
-    s = socket(AF_INET, SOCK_STREAM)  # Создает сокет TCP
-    s.bind((addr, port))  # Присваивает порт 8888
-    s.listen(5)  # Переходит в режим ожидания запросов;
-    s.settimeout(0.2)  # Таймаут для операций с сокетом
-
-    # Одновременно обслуживает не более
-    # 5 запросов.
-    logger.info('Слушаем запросы от клиентов: {0}'.format((addr, port)))
-    # print('Запуск сервера:', (addr, port))
-    while True:
-        try:
-            conn, addr = s.accept()
-        except OSError as e:
-            pass  # timeout вышел
-        else:
-            print("Получен запрос на соединение от %s" % str(addr))
-            logger.info('Получен запрос на соединение от %s' % str(addr))
-            clients.append(conn)
-        finally:
-            wait = 10
-            r = []
-            w = []
-            try:
-                r, w, e = select.select(clients, clients, [], wait)
-            except:
-                pass  # Ничего не делать, если какой-то клиент отключился
-            try:
-                read_requests(r)
-                if r:
-                    print(r)
-                    queue_processing()
-                #result = processing_command(conn, addr)
-                #sending_responde(conn, result)
-            except Exception:
+        def execute_command(command):
+            action = command['action']
+            if action == 'presence':
+                result = self.execute_command_presence(request, command)
+            if action == 'broadcast':
+                result = self.execute_command_broadcast(request, command)
+            elif action == '...':
+                pass
+            else:
                 result = {}
-                result['msg'] = 'Unexpected error'
-                result['code'] = 500
-                sending_responde(conn, result)
-                logger.error('Ошибка обработки запроса клиента {0}: {1}'.format(conn, result['msg']))
+                result['msg'] = 'Invalid command'
+                result['code'] = 400
+            return result
+
+        print('request', request)
+        msg = self.request_queue[request]#request.recv(block_transfer_size).decode('utf-8')
+        print('msg', msg)
+        #addr = request.raddr
+        command = parsing_command(msg)
+        logger.debug('Принято сообщение: {0}, от клиента: {1}'.format(msg, self.addr))
+        logger.info('Принята команда: {0}'.format(command['action']))
+        return execute_command(command)
+
+    def sending_responde(self, client, result):
+        logger.info(
+            'Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
+        # print('Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
+        msg_send = json.dumps(result)
+        client.send(msg_send.encode('utf-8'))
+
+    def sending_responde_brodcaste(self, client, result):
+        logger.info(
+            'Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
+        # print('Возвращаем клиенту {0} msg: {1}; code: {2}'.format(client.getpeername(), result['msg'], result['code']))
+        msg_send = json.dumps(result)
+        for cli in self.clients:
+            cli.send(msg_send.encode('utf-8'))
+        #client.close()
+
+    def read_requests(self, r_clients):
+        """ Чтение запросов из списка клиентов
+        """
+
+        for sock in r_clients:
+            try:
+                data = sock.recv(1024).decode('utf-8')
+                self.request_queue[sock] = data
+            except:
+                print('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
+                logger.info('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
+                self.clients.remove(sock)
+
+    def queue_processing(self):
+        """ Обрабатываем очередь запросов
+        """
+        while self.request_queue != {}:
+            request = next(iter(self.request_queue)) #request_queue[list(request_queue.items())[0]]
+            self.processing_command(request)
+            self.request_queue.pop(request, None)
+
+    def mainloop(self):
+        s = socket(AF_INET, SOCK_STREAM)  # Создает сокет TCP
+        s.bind((self.addr, self.port))  # Присваивает порт 8888
+        s.listen(5)  # Переходит в режим ожидания запросов;
+        s.settimeout(0.2)  # Таймаут для операций с сокетом
+
+        # Одновременно обслуживает не более
+        # 5 запросов.
+        logger.info('Слушаем запросы от клиентов: {0}'.format((self.addr, self.port)))
+        # print('Запуск сервера:', (addr, port))
+        while True:
+            try:
+                conn, addr = s.accept()
+            except OSError as e:
+                pass  # timeout вышел
+            else:
+                print("Получен запрос на соединение от %s" % str(addr))
+                logger.info('Получен запрос на соединение от %s' % str(addr))
+                self.clients.append(conn)
+            finally:
+                wait = 10
+                r = []
+                w = []
+                try:
+                    r, w, e = select.select(self.clients, self.clients, [], wait)
+                except:
+                    pass  # Ничего не делать, если какой-то клиент отключился
+                try:
+                    self.read_requests(r)
+                    if r:
+                        print(r)
+                        self.queue_processing()
+                    #result = processing_command(conn, addr)
+                    #sending_responde(conn, result)
+                except Exception:
+                    result = {}
+                    result['msg'] = 'Unexpected error'
+                    result['code'] = 500
+                    self.sending_responde(conn, result)
+                    logger.error('Ошибка обработки запроса клиента {0}: {1}'.format(conn, result['msg']))
 
 
-logger = logging.getLogger('app.server')
-logger.info('Программа сервер запущена')
+def main():
 
-try:
+    # Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
     parser = create_parser()
     namespace = parser.parse_args(sys.argv[1:])
-    addr = namespace.addr  # 'localhost'
-    port = int(namespace.port)
-    request_queue = {}
-    clients = []
-    mainloop(addr, port)
+    #listen_address, listen_port = namespace.addr, namespace['port']
+
+    # Создание экземпляра класса - сервера.
+    server = Server(namespace.addr, namespace.port)
+    server.mainloop()
+
+
+
+try:
+    # Инициализируем логирование
+    logger = logging.getLogger('app.server')
+    logger.info('Программа сервер запущена')
+
+    if __name__ == '__main__':
+        main()
+
+
+#    parser = create_parser()
+#    namespace = parser.parse_args(sys.argv[1:])
+#    addr = namespace.addr  # 'localhost'
+#    port = int(namespace.port)
+#    request_queue = {}
+#    clients = []
+
 except Exception as err:
-    logger.error(err)
+    if logger:
+        logger.error(err)
+    else:
+        print(err)
